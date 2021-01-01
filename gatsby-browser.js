0fef023d5b3fb091c0583aeccf870c6aa683a71d
Henri Cook <github@henricook.com>
2021-01-01 10:04:40 +0000

    add a prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,7 @@
 import './static/prism-theme.css'
 import { wrapRootElement as wrap } from './src/components/wrap-root-element'
 
+require("prismjs/themes/prism-solarizedlight.css")
+
 // eslint-disable-next-line import/prefer-default-export
 export const wrapRootElement = wrap
